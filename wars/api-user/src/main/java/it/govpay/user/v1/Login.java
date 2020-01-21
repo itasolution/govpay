ddef986a89beeff6c7bf6d702da5fc1b372bed3b
Giuliano Pintori <pintori@link.it>
2020-01-21 18:14:10 +0100

    #154 Modificato il servizio di login per utilizzare un identificativo del servizio richiesto al posto della redirectURL. Aggiunta la possibilita' di configurare le coppie Identificativi/redirectURL da file di properties.

--- wars/api-user/src/main/java/it/govpay/user/v1/Login.java
+++ wars/api-user/src/main/java/it/govpay/user/v1/Login.java
@@ -2,7 +2,7 @@ package it.govpay.user.v1;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
@@ -11,7 +11,6 @@ import javax.ws.rs.core.UriInfo;
 import org.openspcoop2.generic_project.exception.ServiceException;
 
 import it.govpay.rs.v1.BaseRsServiceV1;
-import it.govpay.user.v1.authentication.handler.RedirectAuthenticationSuccessHandler;
 import it.govpay.user.v1.controller.LoginController;
 
 
@@ -30,12 +29,13 @@ public class Login extends BaseRsServiceV1{
 
 
     @GET
-    @Path("/")
+    @Path("/{urlID}")
     
     
-    public Response login(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, @QueryParam(RedirectAuthenticationSuccessHandler.REDIRECT_URL_PARAMETER_NAME) String redirectURL){
+    public Response login(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, @PathParam("urlID") String urlID){
         this.controller.setContext(this.getContext());
-        return this.controller.login(this.getUser(), uriInfo, httpHeaders, redirectURL);
+        this.controller.setRequestResponse(this.request,this.response);
+        return this.controller.login(this.getUser(), uriInfo, httpHeaders,  urlID);
     }
 
 }
